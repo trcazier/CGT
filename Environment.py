f0029d4f9d7133d6f46fd4ad2573a54cc281fa3c
trcazier <tristan.gabriel.r.cazier@vub.be>
2022-12-29 16:57:36 +0100

    [Add] NArmedBanditGame environment

--- Environment.py
+++ Environment.py
@@ -1,49 +1,43 @@
-import networkx as nx
-import random
-import matplotlib.pyplot as plt
+import numpy as np
 
-def generate_graph(n_nodes: int, degree: int):
-    if degree == n_nodes-1:
-        return nx.complete_graph(n_nodes, nx.DiGraph())
-    if degree == 0:
-        return nx.empty_graph(n_nodes, nx.DiGraph())
-    else: 
-        # generate a complete graph and then remove random edges until all
-        # the nodes have the right degree
-        graph = nx.complete_graph(n_nodes, nx.DiGraph())
+CONSTANT = 10
 
-        # list of all the node degrees
-        n_degrees = [n_nodes-1 for _ in range(n_nodes)]
-        # we're done when the list of node degrees is only the degree we want
-        done = lambda: len(list(filter(lambda n: n == degree, n_degrees))) == n_nodes
-        
-        # returns the list of all nodes that are not of the right degree
-        def get_available():
-            available = []
-            for i in range(len(n_degrees)):
-                if n_degrees[i] != degree:
-                    available.append(i)
-            return available
 
-        # remove random edges until all the nodes have the right degree
-        while not done():
-            first = get_available()[0] # choose a node among those with too many edges
-            chosen = random.choice(list(graph.neighbors(first)))
-            if graph.has_edge(first, chosen): # retry if not in the graph
-                graph.remove_edge(first, chosen)
-                n_degrees[first] -= 1
-        
-        return graph
+class NArmedBanditGame:
+    """
+    NArmedBandit Game environment.
+    """
+
+    def __init__(self, num_agents, num_actions):
+        self.num_agents = num_agents
+        self.num_actions = num_actions
+
+        self.rewards = np.random.normal(0, num_agents*CONSTANT, size=(tuple([num_actions for _ in range(num_agents)])))
+
+    def act(self, actions: list[int]):
+        """
+        Method to perform an action in the NArmedBandit Game and obtain the associated reward.
+        :param action: The joint action.
+        :return: The reward.
+        """
+        rewards = self.rewards
+        for i in actions:
+            rewards = rewards[i]
+        return rewards
 
 
 if __name__ == "__main__":
     # test
-    for n in range(5, 10):
-        for deg in range(0, 4):
-            print(f"{n}/10, {deg}/4")
-            for i in range(10000):
-                G = generate_graph(n, deg)
-                for node in list(G.nodes):
-                    assert G.out_degree(node) == deg
-                # nx.draw(G)
-                # plt.show()
+    n_agents = 5
+    n_actions = 4
+    game = NArmedBanditGame(n_agents, n_actions)
+
+    # n_agents number of loops
+    for i in range(n_actions):
+        for j in range(n_actions):
+            for k in range(n_actions):
+                for l in range(n_actions):
+                    for m in range(n_actions):
+                        # also checks the array indexing does not fail
+                        assert game.rewards[i][j][k][l][m] == game.act([i, j, k, l, m])
+    
