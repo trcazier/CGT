e7d29a58f1c9d0031b30ae3a0f9b18901b30739b
trcazier <tristan.gabriel.r.cazier@vub.be>
2022-12-31 19:46:38 +0100

    [Upd] Adapted runner for DCOP

--- DCOP_runner.py
+++ DCOP_runner.py
@@ -1,17 +1,18 @@
+import time
 from typing import Tuple, List
 
 import numpy as np
 from numpy import ndarray
 
-from Graph import generate_random_graph
+from Graph import DCOP_generate_IL, DCOP_generate_JAL, DCOP_generate_JLAL_1, DCOP_generate_JLAL_2, DCOP_generate_JLAL_3
 from LJAL_agent import LJALAgent
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from LJAL_environment import NArmedBanditGame
+from DCOP_environment import DCOPGame
 
 
-def run_episode(env: NArmedBanditGame, agents, training: bool) -> float:
+def run_episode(env: DCOPGame, agents, training: bool) -> float:
     actions = [agent.act(training=training) for agent in agents]
     rew = env.act(actions)
     for i in range(len(agents)):
@@ -23,7 +24,7 @@ def run_episode(env: NArmedBanditGame, agents, training: bool) -> float:
     return rew
 
 
-def train_LJAL(env: NArmedBanditGame, graph, t_max: int) -> Tuple[List[LJALAgent], ndarray]:
+def train_DCOP(env: DCOPGame, graph, t_max: int) -> Tuple[List[LJALAgent], ndarray]:
     """
     Training loop.
 
@@ -35,7 +36,7 @@ def train_LJAL(env: NArmedBanditGame, graph, t_max: int) -> Tuple[List[LJALAgent
     :return: Tuple containing the list of agents, the returns of all training episodes, the averaged evaluation
     return of each evaluation, and the list of the greedy joint action of each evaluation.
     """
-    agents = [LJALAgent(4, list(graph.neighbors(i))) for i in range(5)]
+    agents = [LJALAgent(env.num_actions, list(graph.neighbors(i))) for i in range(env.num_agents)]
     returns = np.zeros(t_max)
     counter = 0
     m = 0
@@ -44,24 +45,39 @@ def train_LJAL(env: NArmedBanditGame, graph, t_max: int) -> Tuple[List[LJALAgent
         returns[counter] = rew
         m = max(rew, m)
         counter += 1
-
     return agents, returns, m
 
 
 if __name__ == '__main__':
-    totals = np.array([np.zeros(200) for i in range(4)])
     ctr = 0
-    labels = ["IQL", "LJAL-2", "LJAL-3", "JAL"]
-    for edges in [0, 2, 3, 4]:
+    
+    num_plays = 200
+    num_agents = 7
+    num_actions = 4
+
+    labels = ["IL", "JAL", "LJAL-1", "LJAL-2", "LJAL-3"]
+    graphs = [
+        DCOP_generate_IL(),
+        DCOP_generate_JAL(),
+        DCOP_generate_JLAL_1(),
+        DCOP_generate_JLAL_2(),
+        DCOP_generate_JLAL_3()
+    ]
+    
+    totals = np.array([np.zeros(num_plays) for _ in range(len(graphs))])
+
+    for graph in graphs:
+        t1 = time.time()
         for i in range(1000):
             if i % 1000 == 0:
                 print(i)
-            env = NArmedBanditGame(5, 4)
-            graph = generate_random_graph(5, edges)
-            agents, returns, m = train_LJAL(env, graph, 200)
+            env = DCOPGame(num_agents, num_actions)
+            agents, returns, m = train_DCOP(env, graph, num_plays)
             totals[ctr] += returns
         totals[ctr] = totals[ctr]/1000
         ctr += 1
+        t2 = time.time()
+        print("time: ", t2-t1)
 
     for i in range(len(totals)):
         plt.plot(totals[i], label=labels[i])
