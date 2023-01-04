Probability normalisation to fix exp overflow

LJAL_agent.py
@@ -20,7 +20,6 @@ class LJALAgent:
         self.q_table = {}
         self.plays = 0
         self.temperature = 1000
-        self.minimum_temperature = 0.01
 
     def init_n_counts(self):
         counts = {}
@@ -54,21 +53,12 @@ class LJALAgent:
         :return: The action.
         """
         self.plays += 1
-        self.temperature = max(1000 * pow(0.94, self.plays), self.minimum_temperature)
+        self.temperature = 1000 * pow(0.94, self.plays)
         evaluations = self.compute_evaluations()
-        probabilities = np.zeros(self.num_actions)
-        to_exp = [ev / self.temperature for ev in evaluations]
-        diff = max(to_exp) - 700
-        if diff > 0:
-            to_exp = list(map(lambda x: x - diff, to_exp))
-        total_sum = sum(np.exp(x) for x in to_exp)
-
-        for action in range(0, self.num_actions):
-            p = np.exp(to_exp[action]) / total_sum
-            if np.isnan(p):
-                probabilities[action] = 1
-            else:
-                probabilities[action] = p
+        exp_evaluations = np.array([ev / self.temperature for ev in evaluations])
+        max_eval = exp_evaluations.max()
+        exp_evaluations = np.exp(exp_evaluations - max_eval)
+        probabilities = exp_evaluations/exp_evaluations.sum()
 
         action = np.random.choice(range(0, self.num_actions), p=probabilities)
         return action

LJAL_runner.py
@@ -1,3 +1,4 @@
+import time
 from typing import Tuple, List
 
 import numpy as np
@@ -48,16 +49,24 @@ def train_LJAL(env: NArmedBanditGame, graph, t_max: int) -> Tuple[List[LJALAgent
 if __name__ == '__main__':
     totals = np.array([np.zeros(200) for i in range(4)])
     ctr = 0
+
+    num_plays = 200
+    num_agents = 5
+    num_actions = 4
+    runs = 100
+
     labels = ["IQL", "LJAL-2", "LJAL-3", "JAL"]
+
     for edges in [0, 2, 3, 4]:
-        for i in range(1000):
-            if i % 1000 == 0:
-                print(i)
-            env = NArmedBanditGame(5, 4)
-            graph = generate_random_graph(5, edges)
-            agents, returns = train_LJAL(env, graph, 200)
+        t1 = time.time()
+        for i in range(runs):
+            env = NArmedBanditGame(num_agents, num_actions)
+            graph = generate_random_graph(num_agents, edges)
+            agents, returns = train_LJAL(env, graph, num_plays)
             totals[ctr] += returns
         totals[ctr] = totals[ctr]/1000
+        t2 = time.time()
+        print(f"{labels[ctr]} time: ", t2-t1)
         ctr += 1
 
     for i in range(len(totals)):